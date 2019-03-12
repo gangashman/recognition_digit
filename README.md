ae668316eb1d45eb1d5f803a9af385be3161e22f
gangashman <ganagsh@gmail.com>
2019-03-12 17:24:31 +0300

    tests

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
-# Dependencies
+# recognition_digit
+
+
+Dependencies
 -----------
 
-PyBrain 
+Install PyBrain 
 ```bash
 git clone git://github.com/pybrain/pybrain.git
 python setup.py install
 ```
 
-MINST library and SciPy
+Other:
 ```bash
 pip install scipy
 pip install mnist
 pip install python-mnist
+pip install matplotlib
 ```
+
+Test
+-----------
+<div align="center"><img src="https://github.com/gangashman/recognition_digit/blob/master/screenshots/Screenshot_20190312_171706.png"/></div>
--- main.py
+++ main.py
@@ -11,11 +11,14 @@ from pybrain.tools.customxml.networkreader import NetworkReader
 from pybrain.tools.customxml.networkwriter import NetworkWriter
 from pybrain.utilities import percentError
 from pybrain.structure import FeedForwardNetwork, FullConnection
+import scipy.misc
 
 
 SIZE = 28 * 28
-HIDDEN_SIZE = 25
+HIDDEN_SIZE = 3
 OUTPUT_SIZE = 10
+
+file_path = 'save.xml'
 
 
 def save(net, file_path):
@@ -42,13 +45,12 @@ def load_or_create(file_path):
         net.addConnection(theta2)
 
         net.sortModules()
+        print("Network created")
         return net
 
 
 mndata = MNIST('data_files')
 images, labels = mndata.load_training()
-
-file_path = 'save.xml'
 net = load_or_create(file_path)
 
 
@@ -64,49 +66,75 @@ true_test = test_ds['target'].argmax(axis=1)
 
 trainer = BackpropTrainer(net, train_ds, learningrate=0.1, momentum=0.1)
 
+print("Size of train_ds: {}".format(len(train_ds['target'])))
+
+
+def get_test_data():
+    outTrain = net.activateOnDataset(train_ds)
+    outTrain = outTrain.argmax(axis=1)
+    resTrain = 100 - percentError(outTrain, true_train)
+
+    outTest = net.activateOnDataset(test_ds)
+    outTest = outTest.argmax(axis=1)
+    resTest = 100 - percentError(outTest, true_test)
+    return resTrain, resTest
+
 
 def test(number):
+    from matplotlib import pyplot as plt
     result = net.activate(images[number])
     print('Result: {}, value: {}'.format(result.argmax(), labels[number]))
 
+    data = [images[number][28 * i: 28 * (i + 1)] for i in range(0, 28)]
 
+    fig, ax = plt.subplots()
+    textstr = '\n'.join((
+        'Result=%s' % result.argmax(),
+        'Value=%s' % labels[number],
+    ))
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14, verticalalignment='top', bbox=props)
+    label = "Result max: {}".format(result.max())
+    ax.text(0.5, -0.1, label, size=12, ha="center", transform=ax.transAxes)
+
+    plt.imshow(data, interpolation='nearest')
+    plt.show()
+
+
+def test_information(f):
+    def new_f(*args, **kwargs):
+        for totalepochs, exec_time, test_data in f(*args, **kwargs):
+            resTrain, resTest = test_data
+            print("".join((
+                "epoch: %3d " % totalepochs,
+                "\ttrain acc: %5.2f%% " % resTrain,
+                "\ttest acc: %5.2f%%" % resTest,
+                "\t{:.3f} seconds".format(exec_time),
+            )))
+    return new_f
+
+
+@test_information
 def train_epochs(epochs):
     for epoch in range(0, epochs):
         start_time = time.time()
         trainer.trainEpochs(1)
-
-        outTrain = net.activateOnDataset(train_ds)
-        outTrain = outTrain.argmax(axis=1)
-        resTrain = 100 - percentError(outTrain, true_train)
-
-        outTest = net.activateOnDataset(test_ds)
-        outTest = outTest.argmax(axis=1)
-        resTest = 100 - percentError(outTest, true_test)
-
         save(net, file_path)
-        print("".join((
-            "epoch: %3d " % trainer.totalepochs,
-            "\ttrain acc: %5.2f%% " % resTrain,
-            "\ttest acc: %5.2f%%" % resTest,
-            "\t{:.3f} seconds".format(time.time() - start_time),
-        )))
+        yield trainer.totalepochs, time.time() - start_time, get_test_data()
 
 
+@test_information
 def train_until_convergence():
-    epochs = 100
-    continue_epochs = 10
-    validation_proportion = 0.15
-
     start_time = time.time()
     train_mse, validation_mse = trainer.trainUntilConvergence(
         verbose=True,
-        validationProportion=validation_proportion,
-        maxEpochs=epochs,
-        continueEpochs=continue_epochs
+        validationProportion=0.80,
+        maxEpochs=100,
+        continueEpochs=10,
+        validationData=test_ds,
     )
-    print(
-        'Training complete; {:.3f} seconds'.format(time.time() - start_time)
-    )
+    save(net, file_path)
+    yield trainer.totalepochs, time.time() - start_time, get_test_data()
 
 
 import pdb; pdb.set_trace()
