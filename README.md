b5b164be8aa89fa1caa5378269aa27ae5e79f490
gangashman <ganagsh@gmail.com>
2019-07-20 17:33:59 +0300

    use sklearn

--- README.md
+++ README.md
@@ -1,19 +1,16 @@
 # recognition_digit
-Digit recognition using PyBrain Machine Learning library trained on MNIST images collection.
+Digit recognition using sklearn library trained on MNIST images collection.
+
+Percentage of correct results: 0.9715
 
 Autor: Nikita Ivanov (Gangashman) ganagsh@gmail.com
 
 Dependencies
 -----------
 
-Install PyBrain:
-```bash
-git clone git://github.com/pybrain/pybrain.git
-python setup.py install
-```
-
 Mnist library and matplotlib for visualization:
 ```bash
+pip install sklearn
 pip install mnist
 pip install python-mnist
 pip install matplotlib
@@ -23,20 +20,12 @@ Tools for working with data:
 ```bash
 sudo apt install gfortran libblas-dev liblapack-dev libatlas-dev
 pip install numpy scipy pillow
-
 ```
 
 Test
 -----------
 Result: network result
 Value: number on image
-~42 seconds per epoch
 
-epoch: 100:
-<div align="center"><img src="https://github.com/gangashman/recognition_digit/blob/master/screenshots/Screenshot_20190313_012706.png"/></div>
+<div align="center"><img src="https://github.com/gangashman/recognition_digit/blob/master/screenshots/Screenshot_20190720_160654.png"/></div>
 
-epoch: 200:
-<div align="center"><img src="https://github.com/gangashman/recognition_digit/blob/master/screenshots/Screenshot_20190313_025324.png"/></div>
-
-epoch: 400:
-<div align="center"><img src="https://github.com/gangashman/recognition_digit/blob/master/screenshots/Screenshot_20190313_053348.png"/></div>
--- main.py
+++ main.py
@@ -1,92 +1,39 @@
 import os
-import time
+import pickle
 
-import scipy
-import scipy.misc
+import numpy as np
 from mnist import MNIST
-from pybrain.datasets import SupervisedDataSet
-from pybrain.structure import (
-    FeedForwardNetwork, FullConnection, LinearLayer, RecurrentNetwork,
-    SigmoidLayer
-)
-from pybrain.structure.modules import SoftmaxLayer
-from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.tools.customxml.networkreader import NetworkReader
-from pybrain.tools.customxml.networkwriter import NetworkWriter
-
-SIZE = 28 * 28
-HIDDEN_SIZE = 25
-OUTPUT_SIZE = 10
-
-file_path = 'save.xml'
-
-
-def save(net, file_path):
-    NetworkWriter.writeToFile(net, file_path)
-
-
-def load_or_create(file_path):
-    if os.path.exists(file_path):
-        net = NetworkReader.readFrom(file_path)
-        print("Network loaded from: {}".format(file_path))
-        return net
-    else:
-        net = RecurrentNetwork()
-
-        inLayer = LinearLayer(SIZE)
-        hiddenLayer = SigmoidLayer(HIDDEN_SIZE)
-        outLayer = SoftmaxLayer(OUTPUT_SIZE)
-
-        net.addInputModule(inLayer)
-        net.addModule(hiddenLayer)
-        net.addOutputModule(outLayer)
-
-        theta1 = FullConnection(inLayer, hiddenLayer)
-        theta2 = FullConnection(hiddenLayer, outLayer)
-
-        net.addConnection(theta1)
-        net.addConnection(theta2)
-
-        net.sortModules()
-        print("Network created")
-        return net
-
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 mndata = MNIST('data_files')
 images, labels = mndata.load_training()
-net = load_or_create(file_path)
 
+file_path = 'save_file'
 
-ds = SupervisedDataSet(SIZE, net.outdim)
+X = []
+y = []
 for image, label in list(zip(images, labels)):
-    values = scipy.zeros(ds.outdim)
-    values[label] = 1
-    ds.addSample(image, values)
-
-train_ds, test_ds = ds.splitWithProportion(0.8)
-
-trainer = BackpropTrainer(net, train_ds, learningrate=0.1, momentum=0.1)
-
-print("Size of train_ds: {}".format(len(train_ds['target'])))
+    X.append(image)
+    y.append(label)
 
 
-def get_test_data(train_count=1000):
-    count = 0
-    for i in range(0, train_count):
-        result = net.activate(images[i])
-        if result.argmax() == labels[i]:
-            count += 1
-    return train_count / float(count)
+X_train, X_test, y_train, y_test = train_test_split(
+    np.array(X), np.array(y), test_size=0.5
+)
 
 
-def test(start_number=0, columns=4, rows=5, fontsize=8):
+def test(classifier, start_number=0, columns=4, rows=5, fontsize=8):
     from matplotlib import pyplot as plt
     ax = []
     fig = plt.figure(figsize=(28, 28))
 
-    for number, i in enumerate(range(start_number, start_number + columns * rows)):
-        result = net.activate(images[i])
-        print('Result: {}, value: {}, {}'.format(result.argmax(), labels[i], result.round(2)))
+    for number, i in enumerate(
+            range(start_number, start_number + columns * rows)
+    ):
+        result = classifier.predict([images[i]])[0]
+
+        print('Result: {}, value: {}'.format(result, labels[i]))
 
         data = [images[i][28 * n: 28 * (n + 1)] for n in range(0, 28)]
 
@@ -94,7 +41,7 @@ def test(start_number=0, columns=4, rows=5, fontsize=8):
         ax.append(fig.add_subplot(rows, columns, number + 1))
 
         textstr = '\n'.join((
-            'Result=%s' % result.argmax(),
+            'Result=%s' % result,
             'Value=%s' % labels[i],
         ))
         props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
@@ -113,38 +60,12 @@ def test(start_number=0, columns=4, rows=5, fontsize=8):
     plt.show()
 
 
-def test_information(f):
-    def new_f(*args, **kwargs):
-        for totalepochs, exec_time, test_data in f(*args, **kwargs):
-            print("".join((
-                "epoch: %3d " % totalepochs,
-                "\ttrain acc: %5.2f%% " % test_data,
-                "\t{:.3f} seconds".format(exec_time),
-            )))
-    return new_f
+if os.path.exists(file_path):
+    classifier = pickle.load(open(file_path, 'rb'))
+else:
+    classifier = KNeighborsClassifier()
+    classifier.fit(X_train, y_train)
+    pickle.dump(classifier, open(file_path, '+wb'))
 
-
-@test_information
-def train_epochs(epochs=100):
-    for epoch in range(0, epochs):
-        start_time = time.time()
-        trainer.trainEpochs(1)
-        save(net, file_path)
-        yield trainer.totalepochs, time.time() - start_time, get_test_data()
-
-
-@test_information
-def train_until_convergence():
-    start_time = time.time()
-    train_mse, validation_mse = trainer.trainUntilConvergence(
-        verbose=True,
-        validationProportion=0.80,
-        maxEpochs=100,
-        continueEpochs=10,
-        validationData=test_ds,
-    )
-    save(net, file_path)
-    yield trainer.totalepochs, time.time() - start_time, get_test_data()
-
-
-import pdb; pdb.set_trace()
+# test(classifier)
+print(classifier.score(X_test, y_test))
